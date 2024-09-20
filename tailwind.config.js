4efa00aafd6117e5b52f9fb6d05ab945879dec67
Arsh <pawar.arsh1333@gmail.com>
2024-09-20 20:44:01 +0530

    added script and made responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,22 +18,22 @@ module.exports = {
         16: "4rem",
       },
     },
-    screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
+    // screens: {
+    //   "2xl": { max: "1535px" },
+    //   // => @media (max-width: 1535px) { ... }
 
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
+    //   xl: { max: "1279px" },
+    //   // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
+    //   lg: { max: "1023px" },
+    //   // => @media (max-width: 1023px) { ... }
 
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
+    //   md: { max: "767px" },
+    //   // => @media (max-width: 767px) { ... }
 
-      sm: { min: "375px" },
-      // => @media (max-width: 639px) { ... }
-    },
+    //   sm: { min: "375px" },
+    //   // => @media (max-width: 639px) { ... }
+    // },
     // colors: {
     //   org: "#ff7757",
     // },
